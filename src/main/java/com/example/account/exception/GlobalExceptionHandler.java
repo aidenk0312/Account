2df63f39939f81b8f9cb6e865610eadd4448b3e3
yongsuKim <yongsu0312.kim@gmail.com>
2023-03-11 13:46:15 +0900

    ver.03111346

---
+++ src/main/java/com/example/account/exception/GlobalExceptionHandler.java
@@ -0,0 +1,46 @@
+package com.example.account.exception;
+
+import com.example.account.dto.ErrorResponse;
+import com.example.account.type.ErrorCode;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import static com.example.account.type.ErrorCode.INTERNAL_SERVER_ERROR;
+import static com.example.account.type.ErrorCode.INVALID_REQUEST;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(AccountException.class)
+    public ErrorResponse handleAccountException(AccountException e) {
+        log.error("{} is occurred.", e.getErrorCode());
+
+        return new ErrorResponse(e.getErrorCode(), e.getErrorMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ErrorResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.error("MethodArgumentNotValidException is occurred.", e);
+
+        return new ErrorResponse(INVALID_REQUEST, INVALID_REQUEST.getDescription());
+    }
+
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ErrorResponse handleDataIntegrityViolationException(DataIntegrityViolationException e) {
+        log.error("DataIntegrityViolationException is occurred.", e);
+
+        return new ErrorResponse(INVALID_REQUEST, INVALID_REQUEST.getDescription());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ErrorResponse handleException(Exception e) {
+        log.error("Exception is occurred.", e);
+
+        return new ErrorResponse(INTERNAL_SERVER_ERROR,
+                INTERNAL_SERVER_ERROR.getDescription());
+    }
+}
